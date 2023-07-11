732053a658dabb4b8d941fb535aa69cd18e46888
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 15:47:53 +0300

    FIR

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,32 @@ export default async function Home() {
 
   const products: Product[] = await res.json();
 
+  const addProductToDatabase = async (e: FormData) => {
+    const product = e.get('product')?.toString();
+    const price = e.get('price')?.toString();
+
+    if(!product || !price) return;
+
+    const newProduct: Product = {
+      product: product,
+      price: price,
+    };
+
+    await fetch('https://64ad4d51b470006a5ec5b8ea.mockapi.io/products', {
+      method: 'POST',
+      body: JSON.stringify(newProduct),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
   return (
     <main>
       <h1 className="text-3xl font-bold text-center">Product Warehouse</h1>
-      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
-        <input placeholder={'Product name'} type="text" className="border border-gray-300 p-2 rounded-md"/>
-        <input placeholder={'Price tag'} type="text" className="border border-gray-300 p-2 rounded-md"/>
+      <form action={addProductToDatabase} className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+        <input name={'product'} placeholder={'Product name'} type="text" className="border border-gray-300 p-2 rounded-md"/>
+        <input name={'price'} placeholder={'Price tag'} type="text" className="border border-gray-300 p-2 rounded-md"/>
         <button className="border bg-blue-500 text-white p-2 rounded-md">Add Product</button>
       </form>
 
